a85490c7be43463b9ad65bb59a606671e33d0642
Vincent Bambico <john.bambico@gameloft.com>
2022-07-14 18:28:05 +0800

    fixed bug with local tv token refresher

--- localtv.php
+++ localtv.php
@@ -1,11 +1,9 @@
 <?php
-  if(!file_exists("localtv_token.txt"))
+  if (!file_exists("localtv_token.txt"))
     include("localtv_auth.php");
 
   $token = file_get_contents("localtv_token.txt");
-
   $channel = "http://103.105.213.251:8443" . $token . $_GET["channel"] . ".m3u8";
-  $context = stream_context_create(['http'=>['ignore_errors'=>true]]);
   $channel_headers = get_headers($channel);
 
   if ($channel_headers[0] == "HTTP/1.1 410 Gone") {
@@ -13,5 +11,5 @@
     $token = file_get_contents("localtv_token.txt");
   }
 
-  header("Location: " . $channel);
+  header("Location: " . "http://103.105.213.251:8443" . $token . $_GET["channel"] . ".m3u8");
 //end localtv.php
