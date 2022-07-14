6432d8f3aa6adf32b558f527407e10b6afe0e87b
Vincent Bambico <john.bambico@gameloft.com>
2022-07-14 22:05:34 +0800

    revamping EpgParser

--- EpgParser.php
+++ EpgParser.php
@@ -1,436 +1,108 @@
 <?php
 
-namespace buibr\xmlepg;
-
-
-use XMLReader;
-use SimpleXMLElement;
-use RuntimeException;
-use DateTimeZone;
-
 class EpgParser {
 
-	//	Source datas.
-	private $file;
-	private $url;
-	private $content;
-
-	//	for temporary file if from content or from url.
-	private $isTemp;
-	public $temp_dir = '/tmp';	//unix, change this for windows.
-
-	//	channel settings
-	private $channels;
-	private $channels_groupby = '@id';
-
-	//	programmes settings.
-	private $epgdata;
-	private $epgdata_groupby = '@id';
-
-	//	filter
-	private $channelfilter = [];
-	private $ignoreDescr = [];
-
-	//	zone.
-	private $targetTimeZone;
-
-	//	callbacks
-	public $onError;
-
-	public function __construct() {
-		$this->targetTimeZone = \date_default_timezone_get();
-	}
-
-	/**
-	 * @param mixed $file`
-	 */
-	public function setFile($file): void {
-		$this->file = $file;
-	}
-
-	/**
-	 * @param mixed $url - url 
-	 */
-	public function setUrl($url): void {
-		$this->url = $url;
-	}
-
-	/**
-	 * @param mixed $content = xml parsed string. 
-	 */
-	public function setContent($content): void {
-		$this->content = $content;
-	}
-
-	/**
-	 * @param mixed $channelfilter
-	 */
-	public function setChannelfilter($channelfilter): void {
-		$this->channelfilter[$channelfilter] = 1;
-	}
-
-	/**
-	 * @param string $descr
-	 */
-	public function setIgnoreDescr(string $descr): void {
-		$this->ignoreDescr[$descr]=1;
-	}
-
-	/**
-	 * @param mixed $targetTimeZone
-	 */
-	public function setTargetTimeZone($targetTimeZone): void {
-		$this->targetTimeZone = $targetTimeZone;
-	}
-
-	/**
-	 * Set group by for channels must be channels atribute.
-	 * @param $group - channel will be grouped with. must be @id or pgram attribute. 
-	 */
-	public function setChannelGroup($group){
-		$this->channels_groupby = $group;
-	}
-
-	/**
-	 * Set group by for channels must be channels atribute.
-	 * 	@id = array index starting from 0
-	 * @param $group - programes will be grouped with. must be @id or pgram attribute.
-	 */
-	public function setProgrammGroup($group){
-		$this->epgdata_groupby = $group;
-	}
-
-	/**
-	 * Parse the date from string in posible formats.
-	 */
-	public function getDate( string $date ){
-
-		try
-		{
-			$dt		= \DateTime::createFromFormat('YmdHis P', $date,new DateTimeZone('UTC'));
-			$dt->setTimezone( new DateTimeZone($this->targetTimeZone) );
-			return	$dt->format('Y-m-d H:i:s');
-		}
-		catch( \Exception $e ){}
-		catch( \Error $e ){}
-
-		try
-		{
-			$dt		= \DateTime::createFromFormat('YmdHis', $date, new DateTimeZone('UTC'));
-			$dt->setTimezone( new DateTimeZone($this->targetTimeZone) );
-			return	$dt->format('Y-m-d H:i:s');
-		}
-		catch( \Exception $e ){}
-		catch( \Error $e ){}
-
-
-		try
-		{
-			$ex = explode(' ', $date);
-			$sd = $ex[0];
-			$ed = $ex[1];
-			
-			if(strlen($sd) == 13) {
-				$sd = "{$sd}0";
-			}
-			
-			$date = $sd." ".$ed;
-
-			$dt		= \DateTime::createFromFormat('YmdHis P', $date,new DateTimeZone('UTC'));
-			$dt->setTimezone( new DateTimeZone($this->targetTimeZone) );
-			return	$dt->format('Y-m-d H:i:s');
-		}
-		catch( \Exception $e ){}
-		catch( \Error $e ){}
-
-		
-		return null;
-	}
-
-	/**
-	 * @param $descr
-	 *
-	 * @return string
-	 */
-	private function filterDescr($descr): string {
-		if (array_key_exists($descr,$this->ignoreDescr)) {
-			return '';
-		}
-		return $descr;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getChannels() {
-		return $this->channels;
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getEpgdata() {
-		return $this->epgdata;
-	}
-
-	/**
-	 * 
-	 */
-	public function resetChannelfilter(): void {
-		$this->channelfilter = [];
-	}
-
-	/**
-	 * 
-	 */
-	private function channelMatchFilter(string $channel): bool {
-		return array_key_exists($channel, $this->channelfilter);
-	}
-
-	/**
-	 * @throws \RuntimeException
-	 * @throws \Exception
-	 */
-	public function parseFile(): void {
-
-		if (!$this->file) {
-			throw new \RuntimeException('missing file: please use setFile before parse');
-		}
-
-		if (!file_exists($this->file)) {
-			throw new \RuntimeException('file does not exists: ' . $this->file);
-		}
-
-		//	
-		$xml = new XMLReader();
-		
-		//	compress.zlib://'
-		$xml->open($this->file);
-
-		/** @noinspection PhpStatementHasEmptyBodyInspection */
-		/** @noinspection LoopWhichDoesNotLoopInspection */
-		/** @noinspection MissingOrEmptyGroupStatementInspection */
-		while ($xml->read() && $xml->name !== 'channel') {
-		}
-
-		$i = 0;
-		while ($xml->name === 'channel') {
-			$element 	= new SimpleXMLElement($xml->readOuterXML());
-
-			/** @noinspection	PhpUndefinedFieldInspection */
-			$group_by	= $this->channels_groupby === '@id' ? (@$i++) : (string)$element->attributes()->{$this->epgdata_groupby};
-
-			//	se the id
-			$channel_id = $group_by?:1;
-
-			/** @noinspection PhpUndefinedFieldInspection */
-			$this->channels[$channel_id] = [
-				'id'=>(string)$element->attributes()->id,
-				'display-name'=>(string)$element->{'display-name'},
-				'url'=>(string)$element->{'url'},
-				'email'=>(string)$element->{'email'},
-				'icon'=> null,
-			];
-
-			if(isset($element->{'icon'}) && $element->{'icon'} instanceof SimpleXMLElement)
-			{
-				$attributes = $element->{'icon'}->attributes();
-				$pathinfo 	= pathinfo($attributes->src);
-
-				if(empty($attributes->src)){
-					$this->channels[$channel_id]['icon'] = (string)$element->{'icon'};
-					$xml->next('channel');
-					unset($element);
-				} 
-				elseif(!filter_var($attributes->src, FILTER_VALIDATE_URL)){
-					$this->channels[$channel_id]['icon'] = (string)$element->{'icon'};
-					$xml->next('channel');
-					unset($element);
-				}
-				elseif(empty($pathinfo['extension'])){
-					$this->channels[$channel_id]['icon'] = (string)$element->{'icon'};
-					$xml->next('channel');
-					unset($element);
-					continue;
-				}
-				else {
-					$this->channels[$channel_id]['icon'] = (string)$attributes->src;
-				}
-			}
-
-			$xml->next('channel');
-			unset($element);
-		}
-
-		$xml->close();
-		$xml->open($this->file);
-
-		/** @noinspection PhpStatementHasEmptyBodyInspection */
-		/** @noinspection LoopWhichDoesNotLoopInspection */
-		/** @noinspection MissingOrEmptyGroupStatementInspection */
-		while ($xml->read() && $xml->name !== 'programme') 
-		{
-		}
-
-		while ($xml->name === 'programme') 
-		{
-			$element = new SimpleXMLElement($xml->readOuterXML());
-
-			/** @noinspection PhpUndefinedFieldInspection */
-			if ( !\count($this->channelfilter) || (\count($this->channelfilter) && $this->channelMatchFilter((string)$element->attributes()->channel))) 
-			{
-
-				/** @noinspection 	PhpUndefinedFieldInspection */
-				$startString		= $this->getDate( (string)$element->attributes()->start );
-				
-				/** @noinspection	PhpUndefinedFieldInspection */
-				$stopString			= $this->getDate( (string)$element->attributes()->stop );
-
-				/** @noinspection	PhpUndefinedFieldInspection */
-				$grouper			= $this->epgdata_groupby === '@id' ? (@$i++) : (string)$element->attributes()->{$this->epgdata_groupby};
-
-				/** @noinspection PhpUndefinedFieldInspection */
-				$this->epgdata[$grouper?:0] = [
-					'start'       => $startString,
-					'start_raw'   => (string)$element->attributes()->start,
-					'channel'     => (string)$element->attributes()->channel,
-					'stop'        => $stopString,
-					'stop_raw'    => (string)$element->attributes()->stop,
-					'title'       => (string)$element->title,
-					'sub-title'   => (string)$element->{'sub-title'},
-					'desc'        => $this->filterDescr((string)$element->desc),
-					'date'        => (int)(string)$element->date,
-					'category'	  => (string)$element->category,
-					'credits'     => (string)$element->credits,
-					'country'     => (string)$element->country,
-					'icon'        => (string)$element->icon,
-					'episode-num' => (string)$element->{'episode-num'},
-				];
-
-			}
-
-			$xml->next('programme');
-			unset($element);
-		}
-
-		$xml->close();
-
-		if($this->isTemp) {
-			@unlink( $this->file );
-		}
-
-	}
-
-	/**
-	 * @throws \RuntimeException
-	 * @throws \Exception
-	 */
-	public function parseUrl(): void {
-
-		if (!$this->url) {
-			throw new \RuntimeException('Url missing: please use setUrl before parseUrl');
-		}
-
-		if (!filter_var($this->url, FILTER_VALIDATE_URL)) {
-			throw new \RuntimeException('Url invalid: ' . $this->url);
-		}
-
-		$this->content = $this->file_get_contents_curl( $this->url );
-		$this->checkXml();
-		$this->saveTemp(); // will save the file.
-		$this->parseFile();
-
-	}
-
-	public function file_get_contents_curl($url, $retries=5)
-	{
-		$ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36';
-
-		if (extension_loaded('curl') === true) {
-			$ch = curl_init();
-
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_USERAGENT, $ua);
-			curl_setopt($ch, CURLOPT_FAILONERROR, TRUE);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-			curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
-
-			$result = curl_exec($ch);
-			curl_close($ch);
-		}
-		else
-			$result = file_get_contents($url);
-
-		if (empty($result) === true) {
-			$result = false;
-
-			if ($retries >= 1) {
-				sleep(1);
-				return file_get_contents_curl($url, --$retries);
-			}
-		}
-
-		return $result;
-	}
-
-	/**
-	 * @throws \RuntimeException
-	 * @throws \Exception
-	 */
-	public function parseContent(): void {
-
-		if (!$this->content) {
-			throw new \RuntimeException('Url missing: please use setUrl before parseUrl');
-		}
-
-		$this->checkXml();
-		$this->saveTemp(); // will save the file.
-		$this->parseFile();
-
-	}
-
-
-	/**
-	 * Save content to temp file from url or from content.
-	 */
-	public function saveTemp( ) : void
-	{
-		if(empty($this->temp_dir)){
-			$this->temp_dir = '/tmp/';
-		}
-
-		$length = 15;
-		$filename = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
-		
-		$this->file = $this->temp_dir . '/' . $filename . '.xml';
-
-		if(!\file_put_contents($this->file, $this->content)){
-			throw new \RuntimeException("Writing to {$this->file} is not possible.");
-		}
-
-		$this->isTemp	= true;
-		$this->content	= null;
-	}
-
-
-	/**
-	 * Check content of response from xml f is xml
-	 * @throws \RuntimeException
-	 */
-	public function checkXml(){
-		libxml_use_internal_errors(true);
-		$doc = simplexml_load_string($this->content);
-		if (!$doc) {
-			$errors = libxml_get_errors();
-			@$errors = $errors[0];
-
-			throw new \RuntimeException("Content of this request its not XML: $errors");
-		}
-	}
-
-
+    // XML parser variables
+    var $parser;
+    var $name;
+    var $attr;
+    var $data  = array();
+    var $stack = array();
+    var $keys;
+    var $path;
+   
+    // either you pass url atau contents.
+    // Use 'url' or 'contents' for the parameter
+    var $type;
+
+    // function with the default parameter value
+    function EpgParser($url='http://www.example.com', $type='url') {
+        $this->type = $type;
+        $this->url  = $url;
+        $this->parse();
+    }
+   
+    // parse XML data
+    function parse()
+    {
+        $data = '';
+        $this->parser = xml_parser_create();
+        xml_set_object($this->parser, $this);
+        xml_set_element_handler($this->parser, 'startXML', 'endXML');
+        xml_set_character_data_handler($this->parser, 'charXML');
+
+        xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+
+        if ($this->type == 'url') {
+            // if use type = 'url' now we open the XML with fopen
+           
+            if (!($fp = @fopen($this->url, 'rb'))) {
+                $this->error("Cannot open {$this->url}");
+            }
+
+            while (($data = fread($fp, 8192))) {
+                if (!xml_parse($this->parser, $data, feof($fp))) {
+                    $this->error(sprintf('XML error at line %d column %d',
+                    xml_get_current_line_number($this->parser),
+                    xml_get_current_column_number($this->parser)));
+                }
+            }
+        } else if ($this->type == 'contents') {
+            // Now we can pass the contents, maybe if you want
+            // to use CURL, SOCK or other method.
+            $lines = explode("\n",$this->url);
+            foreach ($lines as $val) {
+                if (trim($val) == '')
+                    continue;
+                $data = $val . "\n";
+                if (!xml_parse($this->parser, $data)) {
+                    $this->error(sprintf('XML error at line %d column %d',
+                    xml_get_current_line_number($this->parser),
+                    xml_get_current_column_number($this->parser)));
+                }
+            }
+        }
+    }
+
+    function startXML($parser, $name, $attr)    {
+        $this->stack[$name] = array();
+        $keys = '';
+        $total = count($this->stack)-1;
+        $i=0;
+        foreach ($this->stack as $key => $val)    {
+            if (count($this->stack) > 1) {
+                if ($total == $i)
+                    $keys .= $key;
+                else
+                    $keys .= $key . '|'; // The saparator
+            }
+            else
+                $keys .= $key;
+            $i++;
+        }
+        if (array_key_exists($keys, $this->data))    {
+            $this->data[$keys][] = $attr;
+        }    else
+            $this->data[$keys] = $attr;
+        $this->keys = $keys;
+    }
+
+    function endXML($parser, $name)    {
+        end($this->stack);
+        if (key($this->stack) == $name)
+            array_pop($this->stack);
+    }
+
+    function charXML($parser, $data)    {
+        if (trim($data) != '')
+            $this->data[$this->keys]['data'][] = trim(str_replace("\n", '', $data));
+    }
+
+    function error($msg)    {
+        echo "<div align=\"center\">
+            <font color=\"red\"><b>Error: $msg</b></font>
+            </div>";
+        exit();
+    }
 }
+//end EpgParser.php
--- epg.php
+++ epg.php
@@ -1,84 +1,13 @@
 <?php
   require 'EpgParser.php';
 
-  $Parser         = new \buibr\xmlepg\EpgParser();
-  $epgs           = json_decode(file_get_contents($_GET["json"]));
-  $channel_list   = array();
-  $programme_list = array();
+  $url = "https://iptv-org.github.io/epg/guides/ph/clickthecity.com.epg.xml";
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  $tvg = curl_exec($ch);
+  curl_close($ch);
 
-  foreach ($epgs as $epg) {
-    $Parser->setUrl($epg->url);
-    // $Parser->setTargetTimeZone('Asia/Singapore');
-
-    foreach ($epg->channels as $channel)
-      $Parser->setChannelfilter($channel);
-
-    try {
-      $Parser->parseUrl();
-    } catch (Exception $e) {
-      throw new \RuntimeException($e);
-    }
-
-    $channels = array_values($Parser->getChannels());
-
-    foreach ($epg->channels as $channel) {
-      $key = array_search($channel, array_column($channels, "id"));
-
-      if ($key !== false)
-        $channel_list[] = $channels[$key];
-    }
-
-    $programme_list = array_merge($programme_list, $Parser->getEpgdata());
-  }
-
-  $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  $xml .= "<tv date=\"" . date('Ymd') . "\" generator-info-name=\"IPTV-Sniper\">\n";
-
-  foreach ($channel_list as $channel) {
-    $xml .= "  <channel id=\"" . $channel["id"] . "\">\n";
-    $xml .= "    <display-name>". htmlspecialchars($channel["display-name"]) . "</display-name>\n";
-    $xml .= "    <icon src=\"" . $channel["icon"] . "\" />\n";
-    $xml .= "    <url>" . $channel["url"] . "</url>\n";
-    $xml .= "  </channel>\n";
-  }
-
-  $event = json_decode(file_get_contents("special_event.json"));
-
-  if (is_object($event)) {
-    $xml .= "  <channel id=\"SpecialEvents\">\n";
-    $xml .= "    <display-name>Special Events</display-name>\n";
-    $xml .= "    <icon src=\"https://i.imgur.com/vRlLmha.png\" />\n";
-    $xml .= "    <url>https://iptv-sniper.herokuapp.com/</url>\n";
-    $xml .= "  </channel>\n";
-  }
-
-  foreach ($programme_list as $programme) {
-    $xml .= "  <programme start=\"" . $programme["start_raw"] . "\" stop=\"" . $programme["stop_raw"] . "\" channel=\"" . $programme["channel"] . "\">\n";
-    $xml .= "    <title lang=\"en\">" . htmlspecialchars($programme["title"]) . "</title>\n";
-    $xml .= "    <desc lang=\"en\">" . htmlspecialchars($programme["desc"]) . "</desc>\n";
-    $xml .= "    <category lang=\"en\">" . htmlspecialchars($programme["category"]) . "</category>\n";
-    $xml .= "  </programme>\n";
-  }
-
-  if (is_object($event)) {
-    date_default_timezone_set($event->timezone);
-
-    $event_start = DateTime::createFromFormat('ga F j, Y', $event->start)
-                           ->setTimezone(new DateTimeZone("Asia/Singapore"))
-                           ->format("YmdHis O");
-
-    $event_end   = DateTime::createFromFormat('ga F j, Y', $event->end)
-                           ->setTimezone(new DateTimeZone("Asia/Singapore"))
-                           ->format("YmdHis O");
-
-    $xml .= "  <programme start=\"" . $event_start . "\" stop=\"" . $event_end . "\" channel=\"SpecialEvents\">\n";
-    $xml .= "    <title lang=\"en\">" . htmlspecialchars($event->title) . "</title>\n";
-    $xml .= "    <desc lang=\"en\">" . htmlspecialchars($event->description) . "</desc>\n";
-    $xml .= "    <category lang=\"en\">" . htmlspecialchars($event->category) . "</category>\n";
-    $xml .= "  </programme>\n";
-  }
-
-  $xml .= "</tv>";
-  $filename = ($_GET["json"] == "epg_config.json") ? "iptv-sniper.xml" : "cryogenix.xml";
-  file_put_contents($filename, $xml);
+  $epgs = new EpgParser($tvg);
+  die("<pre>" . print_r($epgs, true) . "</pre>");
 //end epg.php
