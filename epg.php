3227e1e58ef1be34ac995d055f8c3b2110224007
Vincent Bambico <john.bambico@gameloft.com>
2022-07-15 13:17:25 +0800

    test channels writing to epg

--- epg.php
+++ epg.php
@@ -1,17 +1,68 @@
 <?php
   $epgs = json_decode(file_get_contents($_GET["json"]));
-  $programme_list = array();
 
   foreach ($epgs as $epg) {
+    $channels_list = getChannels($epg->url, $epg->channes);
+    $programme_list = getProgrammes($epg->url, $epg->channels);
+  }
+
+  $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  $xml .= "<tv date=\"" . date('Ymd') . "\" generator-info-name=\"IPTV-Sniper\">\n";
+
+  foreach ($epgs as $epg) {
+    foreach ($channels_list as $channel) {
+      $xml .= "  <channel id=\"" . $channel["id"] . "\">\n";
+      $xml .= "    <display-name>". htmlspecialchars($channel["display-name"]) . "</display-name>\n";
+      $xml .= "    <icon src=\"" . $channel["icon"] . "\" />\n";
+      $xml .= "    <url>" . $channel["url"] . "</url>\n";
+      $xml .= "  </channel>\n";
+    }
+  }
+
+  $xml .= "</tv>";
+  echo $xml;
+
+  function getChannels($url, $channels) {
+    $channels_list = array();
+
     $xml = new XMLReader();
-    $xml->open("compress.zlib://" . $epg_json->url);
+    $xml->open("compress.zlib://" . $url);
+
+    while ($xml->read() && $xml->name !== 'channel') {
+    }
+
+    while ($xml->name === 'channel') {
+      $channel = new SimpleXMLElement($xml->readOuterXML());
+      $key = array_search(strval($channel->attributes()->id), $channels);
+
+      if ($key !== false)
+        $channels_list[strval($channel->attributes()->id)] = [
+          "id"           => strval($channel->attributes()->id),
+          "display-name" => strval($channel->{'display-name'}),
+          "icon"         => strval($channel->{'icon'}->attributes->src),
+          "url"          => strval($channel->{'url'})
+        ];
+
+      $xml->next('channel');
+      unset($channel);
+    }
+
+    $xml->close();
+    return $channels_list;
+  }
+
+  function getProgrammes($url, $channels) {
+    $programme_list = array();
+
+    $xml = new XMLReader();
+    $xml->open("compress.zlib://" . $url);
 
     while ($xml->read() && $xml->name !== 'programme') {
     }
 
     while ($xml->name === 'programme') {
       $programme = new SimpleXMLElement($xml->readOuterXML());
-      $key = array_search(strval($programme->attributes()->channel), $epg->channels);
+      $key = array_search(strval($programme->attributes()->channel), $channels);
 
       if ($key !== false)
         $programme_list[strval($programme->attributes()->channel)] = [
@@ -28,21 +79,6 @@
     }
 
     $xml->close();
+    return $programme_list;
   }
-
-  $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  $xml .= "<tv date=\"" . date('Ymd') . "\" generator-info-name=\"IPTV-Sniper\">\n";
-
-  foreach ($epgs as $epg) {
-    foreach ($epg->channels as $channel) {
-      $xml .= "  <channel id=\"" . $channel["id"] . "\">\n";
-      $xml .= "    <display-name>". htmlspecialchars($channel["display-name"]) . "</display-name>\n";
-      $xml .= "    <icon src=\"" . $channel["icon"] . "\" />\n";
-      $xml .= "    <url>" . $channel["url"] . "</url>\n";
-      $xml .= "  </channel>\n";
-    }
-  }
-
-  $xml .= "</tv>";
-  echo $xml;
 //end epg.php
