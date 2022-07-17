a0c563af4564a36357d1ca31e52332f9d4f79f13
Vincent Bambico <john.bambico@gameloft.com>
2022-07-17 18:19:55 +0800

    modified channel source checker

--- tasks/UrlTester.php
+++ tasks/UrlTester.php
@@ -6,10 +6,14 @@ use Amp\Parallel\Worker\Task;
 
 class UrlTester implements Task
 {
-  private $url;
+  private $url, $channel_id, $channel_name, $channel_logo, $channel_group;
 
-  public function __construct($url) {
+  public function __construct($url, $channel) {
     $this->url = $url;
+    $this->channel_id = $channel->id;
+    $this->channel_name = $channel->name;
+    $this->channel_logo = $channel->logo;
+    $this->channel_group = $channel->group;
   }
 
   public function run(Environment $environment) {
@@ -21,6 +25,12 @@ class UrlTester implements Task
     curl_exec($curl);
     $result = curl_getinfo($curl);
     curl_close($curl);
-    return yield $result["http_code"];
+
+    $status = ($result["http_code"] == "200") ? "[✓] " : "[✖] ";
+    $m3u = "#EXTINF:-1 tvg-id=\"$this->channel_id\" tvg-name=\"$this->channel_name\" tvg-logo=\"$this->channel_logo\" group-title=\"$this->channel_group\",$status$this->channel_name\n";
+    $m3u .= $this->url . "\n\n";
+
+    file_put_contents("nette.safe://tv_channels.m3u", $m3u, FILE_APPEND);
+    return "M3U processing for $this->url complete.\n";
   }
 }
