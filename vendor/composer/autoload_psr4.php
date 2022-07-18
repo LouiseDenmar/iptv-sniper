50ed6515d7ab4c9f0647782e426e13944d7cd2ed
BambicoIsaac <isaacbambico@gmail.com>
2022-07-18 14:42:04 +0800

    removed concurrency implementation for m3u generator

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,12 +6,4 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Tasks\\' => array($baseDir . '/tasks'),
-    'Amp\\Sync\\' => array($vendorDir . '/amphp/sync/src'),
-    'Amp\\Serialization\\' => array($vendorDir . '/amphp/serialization/src'),
-    'Amp\\Process\\' => array($vendorDir . '/amphp/process/lib'),
-    'Amp\\Parser\\' => array($vendorDir . '/amphp/parser/lib'),
-    'Amp\\Parallel\\' => array($vendorDir . '/amphp/parallel/lib'),
-    'Amp\\ByteStream\\' => array($vendorDir . '/amphp/byte-stream/lib'),
-    'Amp\\' => array($vendorDir . '/amphp/amp/lib'),
 );
