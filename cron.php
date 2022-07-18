78315a2c8e278e84dae5d40c3940cbf851c5116a
Vincent Bambico <john.bambico@gameloft.com>
2022-07-18 21:46:01 +0800

    initial database setup

--- cron.php
+++ cron.php
@@ -1,4 +1,15 @@
 <?php
-  file_put_contents(__DIR__ . "/test.text", "Hello world!");
-  echo __DIR__;
-//end tv_channels.php
+  $conn = new mysqli_connect("lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "nsvwm2u733p490gm", "kdj0cijcfrceu8gk", "ofagdoh1n25jv0tw");
+
+  if ($conn->connect_error)
+    die("Connection failed: " . $conn->connect_error);
+
+  $sql = "CREATE TABLE files (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(30) NOT NULL,
+    file BLOB NOT NULL
+  )";
+
+  echo ($conn->query($sql) === TRUE) ? "Table files created successfully!" : "Error creating table: " . $conn->error;
+  $conn->close();
+//end cron.php
