f38578bb2194bd741443baac5c8b6c775921d666
Vincent Bambico <john.bambico@gameloft.com>
2022-07-19 22:09:10 +0800

    trying to fetch saved m3u data

--- tv_channels.php
+++ tv_channels.php
@@ -13,6 +13,6 @@
     die("Connection failed: " . $conn->connect_error);
 
   $result = $conn->query("SELECT UNCOMPRESS(file) FROM files WHERE filename='tv_channels.m3u' LIMIT 1")->fetch_row();
-  echo ($result === TRUE) ? $result[0] : $conn->error;
+  var_dump($result);
   $conn->close();
 //end tv_channels.php
