7a657fdafe85eebba67dec3ed12fb1e9a44a52c5
BambicoIsaac <isaacbambico@gmail.com>
2022-07-19 21:36:56 +0800

    added m3u db fetching logic

--- tv_channels.php
+++ tv_channels.php
@@ -1,25 +1,18 @@
 <?php
-  $sql = "SELECT UNCOMPRESS(file) FROM files WHERE filename='tv_channels.m3u'";
-  $result = query($sql);
-  $m3u = $result->fetch_row();
-  echo $m3u[0];
+  $url = getenv('JAWSDB_MARIA_URL');
+  $dbparts = parse_url($url);
 
-  function query($sql) {
-    $url = getenv('JAWSDB_MARIA_URL');
-    $dbparts = parse_url($url);
+  $hostname = $dbparts['host'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'],'/');
 
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
+  $conn = new mysqli($hostname, $username, $password, $database);
 
-    $conn = new mysqli($hostname, $username, $password, $database);
+  if ($conn->connect_error)
+    die("Connection failed: " . $conn->connect_error);
 
-    if ($conn->connect_error)
-      die("Connection failed: " . $conn->connect_error);
-
-    $result = $conn->query($sql);
-    $conn->close();
-    return ($result === TRUE) ? $result : $conn->error;
-  }
+  $result = $conn->query("SELECT UNCOMPRESS(file) FROM files WHERE filename='tv_channels.m3u'")->fetch_row();
+  $conn->close();
+  echo ($result === TRUE) ? $result : $conn->error;
 //end tv_channels.php
