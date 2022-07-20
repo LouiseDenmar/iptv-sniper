07dd766dc310402f9369d0c1cb8fc6cc91d68fba
BambicoIsaac <isaacbambico@gmail.com>
2022-07-20 08:21:40 +0800

    modified database insertion routine for epg updater

--- adoboTV.php
+++ adoboTV.php
@@ -12,13 +12,11 @@
   if ($conn->connect_error)
     die("Connection failed: " . $conn->connect_error);
 
-  echo $conn->query("SELECT * FROM files WHERE id=2 LIMIT 1")
-            ->fetch_object()
-            ->filename;
+  var_dump($conn->query("SELECT * FROM files WHERE id=2 LIMIT 1")->fetch_object());
 
-  echo $conn->query("SELECT UNCOMPRESS(file) AS file FROM files WHERE filename='adoboTV.xml' LIMIT 1")
-            ->fetch_object()
-            ->file;
+//   echo $conn->query("SELECT UNCOMPRESS(file) AS file FROM files WHERE filename='adoboTV.xml' LIMIT 1")
+//             ->fetch_object()
+//             ->file;
 
   $conn->close();
 //end adoboTV.php
